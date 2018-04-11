767c7d1a2bdf9345daf8cf245796ac3a045218be
longjump2k <38274218+longjump2k@users.noreply.github.com>
2018-04-11 01:59:08 -0400

    Add files via upload

---
+++ Exit_Screen.java
@@ -0,0 +1,29 @@
+package com.example.android.subiequiz;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Exit_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exit__screen);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+}
+
+}
