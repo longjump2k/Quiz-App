275cbc5fe26914b5d1004148d8f949be98168b19
longjump2k <38274218+longjump2k@users.noreply.github.com>
2018-04-11 21:34:46 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -104,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // Checks the EditText answer for question 6
+    // Checks the EditText answer for question 6
     public int editTextQ6 () {
         // Import the editText for question 6
         EditText editTextQ6Correct = findViewById(R.id.questionSixText);
